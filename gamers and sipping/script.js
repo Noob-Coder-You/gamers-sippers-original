e8c4956064512dac9daad5f2199a01a6cfa038cf
Noob-Coder-You <88716832+Noob-Coder-You@users.noreply.github.com>
2021-09-28 20:44:41 +0530

    Add files via upload

---
+++ gamers and sipping/script.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.burger')
+const toggle = document.querySelector('.toggle')
+const nav = document.querySelector('.nav')
+
+
+
+burger.addEventListener('click',()=>{
+
+nav.classList.toggle('toggle')
+
+
+
+})
